39f929ffd8c58063cf737ad6f3c989d62f7b01f4
95degree <95degree>
2020-11-04 15:48:00 +0900

    new

--- day3/Indian_name.java
+++ day3/Indian_name.java
@@ -1,178 +1,34 @@
 import java.util.Scanner;
 
 public class Indian_name {
+
+    public String Year_printout(int Input_year){
+        String[] Year_Name = {"말 많은", "푸른", "어두운", "조용한", "웅크린", "백색",
+                "지혜로운", "용감한", "날카로운", "욕심많은"};
+        return Year_Name[Input_year%10];
+    }
+    public String Month_printout(int Input_Month){
+        String[] Month_Name = {"늑대", "태양", "양", "매", "황소", "불꽃", "나무", "달빛",
+                "말", "돼지", "하늘", "바람"};
+        return Month_Name[Input_Month-1];
+    }
+    public String Day_printout(int Input_Day){
+        String[] Day_Name = {"와(과) 함께춤을", "의 기상", "","","", "의 환생", "의 죽음",
+                         "아래에서", "를(을) 보라", "이(가) 노래하다.", "그림자", "의 일격",
+                         "에게 쫓기는 남자", "의 행진", "의 왕", "의 유령", "을 죽인자","는(은) 맨날 잠잔다.",
+                         "처럼","의 고향", "의 전사", "은(는) 나의 친구", "의 노래", "의 정령", "의 파수꾼",
+                         "의 악마", "와(과) 같은 사나이", "를(을) 쓰러트린자", "의 혼", "은(는) 말이 없다."};
+        return Day_Name[Input_Day-1];
+    }
+
     public static void main(String[] args) {
-        System.out.println("생년월일을 입력해 주세요 >");
+        Indian_name name = new Indian_name();
         Scanner Input = new Scanner(System.in);
-        int year = Input.nextInt();
+        System.out.println("생년월일을 입력해 주세요 >");
+        int Year = Input.nextInt();
         int Month = Input.nextInt();
         int Day = Input.nextInt();
-        String Year_name = "", Month_name = "", Day_name = "";
         Input.close();
-
-        year = year%10 ;
-        switch(year){
-            case 0 :
-                Year_name = "말 많은";
-                break;
-            case 1 :
-                Year_name = "푸른";
-                break;
-            case 2 :
-                Year_name = "어두운";
-                break;
-            case 3 :
-                Year_name = "조용한";
-                break;
-            case 4 :
-                Year_name = "웅크린";
-                break;
-            case 5 :
-                Year_name = "백새";
-                break;
-            case 6 :
-                Year_name = "지혜로운";
-                break;
-            case 7 :
-                Year_name = "용감한";
-                break;
-            case 8 :
-                Year_name = "날카로운";
-                break;
-            case 9 :
-                Year_name = "욕심많은";
-                break;
-        }
-        switch(Month){
-            case 1 :
-                Month_name = "늑대";
-                break;
-            case 2 :
-                Month_name = "태양";
-                break;
-            case 3 :
-                Month_name = "양";
-                break;
-            case 4 :
-                Month_name = "매";
-                break;
-            case 5 :
-                Month_name = "황소";
-                break;
-            case 6 :
-                Month_name = "불꽃";
-                break;
-            case 7 :
-                Month_name = "나무";
-                break;
-            case 8 :
-                Month_name = "달빛";
-                break;
-            case 9 :
-                Month_name = "말";
-                break;
-            case 10 :
-                Month_name = "돼지";
-                break;
-            case 11:
-                Month_name = "하늘";
-                break;
-            case 12:
-                Month_name = "바람";
-                break;
-        }
-
-         switch(Day){
-             case 1 :
-                 Day_name = "와(과) 함께 춤을";
-                 break;
-             case 2 :
-                 Day_name = "의 기상";
-                 break;
-             case 3 :
-                 Day_name = "은(는) 그림자속에";
-                 break;
-             case 4 :
-             case 5 :
-             case 6 :
-                 Day_name = "";
-                 break;
-             case 7 :
-                 Day_name = "의 환생";
-                 break;
-             case 8 :
-                 Day_name= "의 죽음";
-                 break;
-             case 9 :
-                 Day_name = "아래에서";
-                 break;
-             case 10 :
-                 Day_name = "를(을) 보라";
-                 break;
-             case 11:
-                 Day_name = "이(가) 노래하다";
-                 break;
-             case 12:
-                 Day_name = "의 그림자";
-                 break;
-             case 13:
-                 Day_name = "의 일격";
-                 break;
-             case 14:
-                 Day_name = "에게 쫓기는 남자";
-                 break;
-             case 15 :
-                 Day_name = "의 행진";
-                 break;
-             case 16 :
-                 Day_name = "의 왕";
-                 break;
-             case 17 :
-                 Day_name = "의 유령";
-                 break;
-             case 18 :
-                 Day_name = "을 죽인자";
-                 break;
-             case 19 :
-                 Day_name = "는(은) 맨날 잠잔다";
-                 break;
-             case 20 :
-                 Day_name = "처럼";
-                 break;
-             case 21 :
-                 Day_name = "의 고향";
-                 break;
-             case 22 :
-                 Day_name = "의 전사";
-                 break;
-             case 23 :
-                 Day_name = "은(는) 나의 친구";
-                 break;
-             case 24 :
-                 Day_name = "의 노래";
-                 break;
-             case 25 :
-                 Day_name = "의 정령";
-                 break;
-             case 26 :
-                 Day_name = "의 파수꾼";
-                 break;
-             case 27 :
-                 Day_name = "의 악마";
-                 break;
-             case 28 :
-                 Day_name = "와(과) 같은 사나이";
-                 break;
-             case 29 :
-                 Day_name = "를(을) 쓰러트린자";
-                 break;
-             case 30 :
-                 Day_name = "의 혼";
-                 break;
-             case 31 :
-                 Day_name = "은(는) 말이 없다";
-                 break;
-        }
-        System.out.println("당신의 이름은 "+Year_name+" "+Month_name+Day_name+"입니다.");
+        System.out.println(name.Year_printout(Year)+" "+name.Month_printout(Month)+name.Day_printout(Day));
     }
 }
