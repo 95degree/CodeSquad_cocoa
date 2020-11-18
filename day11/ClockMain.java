40d1dc0c46cb05a30087cc69c6de33c734973a93
95degree <95degree>
2020-11-18 15:30:44 +0900

    한글시계

--- day11/ClockMain.java
+++ day11/ClockMain.java
@@ -1,18 +1,9 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class ClockMain {
 
-    public String makeNowTime(){
-        Date now = new Date();
-        SimpleDateFormat time = new SimpleDateFormat("h:m");
-        return time.format(now);
-    }
 
     public static void main(String[] args) {
-        ClockMain clock = new ClockMain();
         ClockKorean korean = new ClockKorean();
-        Time time = new Time (clock.makeNowTime());
+        Time time = new Time();
         korean.init(time.getHour(), time.getMinute());
     }
 }
--- day11/Time.java
+++ day11/Time.java
@@ -1,13 +1,22 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Time {
 
     private int hour;
     private int minute;
 
-    public Time(String time) {
+
+    public Time() {
         String[] hourAndMinute = new String[2];
-        hourAndMinute = time.split(":");
+        hourAndMinute = makeNowTime().split(":");
         this.hour = Integer.parseInt(hourAndMinute[0]);
         this.minute = Integer.parseInt(hourAndMinute[1]);
+    }
+    public String makeNowTime(){
+        Date now = new Date();
+        SimpleDateFormat time = new SimpleDateFormat("h:m");
+        return time.format(now);
     }
 
     public int getHour(){
