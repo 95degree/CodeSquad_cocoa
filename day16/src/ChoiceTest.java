5adf47cae3cee9d90e308f7e6c713967ac5cf2d3
95degree <95degree@naver.com>
2020-11-23 12:14:25 +0900

    AWT 공부

--- day16/src/ChoiceTest.java
+++ day16/src/ChoiceTest.java
@@ -1,2 +1,21 @@
+import java.awt.*;
+
 public class ChoiceTest {
+    public static void main(String[] args) {
+        Frame f = new Frame("Choice - Select");
+        f.setSize(300,200);
+        f.setLayout(null);
+
+        Choice day = new Choice();
+        String[]date = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
+
+        for(String a : date)
+        day.add(a);
+
+        day.setSize(100,50);
+        day.setLocation(100,70);
+
+        f.add(day);
+        f.setVisible(true);
+    }
 }
