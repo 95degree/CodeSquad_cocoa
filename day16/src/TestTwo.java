76cd6b9588260a7c0e6f0d428e906ba4ce7f2d67
95degree <95degree@naver.com>
2020-11-23 11:47:47 +0900

    AWT 공부

--- day16/src/TestTwo.java
+++ day16/src/TestTwo.java
@@ -1,2 +1,13 @@
+import java.awt.*;
+
 public class TestTwo {
+    public static void main(String[] args) {
+        Frame f = new Frame("Login");
+        f.setSize(300,200);
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+
+        f.setLocation(screenSize.width/2-150, screenSize.height/2-100);
+        f.setVisible(true);
+    }
 }
