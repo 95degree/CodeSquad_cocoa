678e82b0c73c3803dbab8bac1f393a44cef8b908
95degree <95degree>
2020-11-23 11:30:22 +0900

    AWT 공부

---
+++ day16/src/test.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class test {
+    public static void main(String[] args) {
+        Frame f = new Frame("Login");
+        f.setSize(300,200);
+        f.setVisible(true);
+    }
+}
