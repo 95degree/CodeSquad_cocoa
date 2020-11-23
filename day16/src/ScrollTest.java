b36036e7a4d709ed3a8f9ae1f1af27c46c1705ff
95degree <95degree@naver.com>
2020-11-23 14:42:58 +0900

    AWT 공부

---
+++ day16/src/ScrollTest.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class ScrollTest {
+    public static void main(String[] args) {
+        Frame f = new Frame("Scroll");
+        f.setSize(300,200);
+        f.setLayout(null);
+
+        Scrollbar hor = new Scrollbar(Scrollbar.HORIZONTAL, 50, 20, 0, 100);
+        hor.setSize(100,15);
+        hor.setLocation(100,100);
+        Scrollbar ver = new Scrollbar(Scrollbar.VERTICAL, 50, 20, 0, 100);
+        ver.setSize(15, 100);
+        ver.setLocation(30,60);
+
+        f.add(hor);
+        f.add(ver);
+        f.setVisible(true);
+    }
+}
+
