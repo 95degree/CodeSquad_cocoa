af6574ada68a800be8b7ebf088a45a0e14092411
95degree <95degree@naver.com>
2020-11-23 11:39:04 +0900

    AWT 공부

--- day16/src/test.java
+++ day16/src/test.java
@@ -4,6 +4,6 @@ public class test {
     public static void main(String[] args) {
         Frame f = new Frame("Login");
         f.setSize(300,200);
-        f.setVisible(true);
+        //f.setVisible(true);
     }
 }
