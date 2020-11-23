653f7fba5c20c3ebe037d961303790d0f5817903
95degree <95degree@naver.com>
2020-11-23 14:44:36 +0900

    AWT 공부

--- day16/src/ScrollTest.java
+++ day16/src/ScrollTest.java
@@ -6,10 +6,10 @@ public class ScrollTest {
         f.setSize(300,200);
         f.setLayout(null);
 
-        Scrollbar hor = new Scrollbar(Scrollbar.HORIZONTAL, 50, 20, 0, 100);
+        Scrollbar hor = new Scrollbar(Scrollbar.HORIZONTAL, 0, 20, 0, 100);
         hor.setSize(100,15);
         hor.setLocation(100,100);
-        Scrollbar ver = new Scrollbar(Scrollbar.VERTICAL, 50, 20, 0, 100);
+        Scrollbar ver = new Scrollbar(Scrollbar.VERTICAL, 0, 20, 0, 100);
         ver.setSize(15, 100);
         ver.setLocation(30,60);
 
