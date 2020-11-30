73de38b0d3e81037d0278ddfd31747600c2d5a6c
95degree <95degree@naver.com>
2020-11-30 15:06:23 +0900

    개인프로젝트

--- day21/src/GameMain.java
+++ day21/src/GameMain.java
@@ -1,5 +1,6 @@
 public class GameMain {
     public static void main(String[] args) {
-
+        Map map = new Map();
+        map.printMap();
     }
 }
--- day21/src/Map.java
+++ day21/src/Map.java
@@ -3,27 +3,53 @@ import java.util.Random;
 public class Map {
     private final static int SIZE = 4;
     private int[][] map;
-    private int[] number;
 
     public Map() {
-        this.number = new int[SIZE * SIZE];
         init();
     }
 
     private void init() {
-        inputNum();
-        inputRandomNum();
+        int[] number = new int[SIZE * SIZE];
+        inputNum(number);
+        makeMap(number);
     }
 
-    private void inputNum() {
+    private void inputNum(int[] number) {
+        Random random = new Random();
         for (int i = 0; i < SIZE * SIZE; i++) {
-            number[i] = i + 1;
+            number[i] = i ;
+        }
+        for (int i = number.length - 1; i > 0; i--) {
+            shuffle(number, i, random.nextInt(i));
         }
     }
 
-    private void inputRandomNum() {
-        Random random = new Random();
-        for (int a : number) {
+    private void shuffle(int[] arrayNumber, int i, int randomNumber) {
+        int save = arrayNumber[i];
+        arrayNumber[i] = arrayNumber[randomNumber];
+        arrayNumber[randomNumber] = save;
+    }
+
+    private void makeMap(int[] arrayNumber) {
+        this.map = new int[SIZE][SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                this.map[i][j] = arrayNumber[i * SIZE + j];
+            }
+        }
+    }
+
+    public int[][] getMap() {
+        return this.map;
+    }
+
+    public void printMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.printf("%3d", this.map[i][j]);
+            }
+            System.out.println();
         }
     }
 }
